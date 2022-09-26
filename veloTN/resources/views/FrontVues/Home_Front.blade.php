2f61cc75767780e2fc975c2a1f9915a019b127e5
elyes10 <47626987+elyes10@users.noreply.github.com>
2022-09-26 02:23:16 +0100

    first commit
    template integrated

---
+++ veloTN/resources/views/FrontVues/Home_Front.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.FrontMainLayout')
+@section('title', 'Home')
+<div>
+    @section('navbar')
+        @parent
+    @endsection
+</div>
+
+<div>
+
+    @section('content')
+        <p>HI elyes</p>
+
+    @endsection
+</div>
+
+
